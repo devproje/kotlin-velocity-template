f3468209ac80d6e6831ec3ef15c186370aa7c0f0
Project_TL <me@projecttl.net>
2022-08-15 00:45:54 +0900

    fix: add @Inject annotation

--- src/main/kotlin/net/projecttl/velocity/example/ProxyCore.kt
+++ src/main/kotlin/net/projecttl/velocity/example/ProxyCore.kt
@@ -1,12 +1,13 @@
 package net.projecttl.velocity.example
 
+import com.google.inject.Inject
 import com.velocitypowered.api.proxy.ProxyServer
 import net.projecttl.velocity.example.commands.TestCommand
 import org.slf4j.Logger
 
 lateinit var instance: ProxyServer
 
-class ProxyCore(server: ProxyServer, logger: Logger) {
+class ProxyCore @Inject constructor(server: ProxyServer, logger: Logger) {
     init {
         instance = server
         server.commandManager.apply {
